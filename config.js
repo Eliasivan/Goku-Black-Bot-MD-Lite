6111bea7c20c563924e5fc60ab0b91dcf0212683
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2025-05-28 18:33:50 -0300

    Crear config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    botName: 'Goku-Black-Bot-MD-Lite',
+    owner: 'Eliasivan',
+    creator: 'Ivan',
+    version: '1.0.0',
+    sessionPath: './auth_info_baileys',
+    defaultPrefix: '!',
+    qrTimeout: 30000,
+    pairingCodeTimeout: 30000,
+    colors: {
+        success: 'green',
+        error: 'red',
+        info: 'cyan'
+    }
+};
